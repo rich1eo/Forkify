67ef46e445c96c4f582bc3384f287a3ce48fcd19
Vladislav Rychkov <v.richkov20@gmail.com>
2023-01-29 20:00:02 +0300

    feat: handle errors

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,14 +4,6 @@ import recipeView from './views/recipeView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
@@ -21,18 +13,19 @@ const controlRecipes = async function () {
     const id = window.location.hash.slice(1);
     if (!id) return;
 
-    recipeView.renderSpinner();
-
     // 1) Loading recipe
+    recipeView.renderSpinner();
     await model.loadRecipe(id);
 
     // 2) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
     console.error(err);
+    recipeView.renderError();
   }
 };
 
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
